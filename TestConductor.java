c018431862754768c93844a66f5db755cdc635af
John <jbotonakis@carroll.edu>
2025-03-23 17:11:14 -0600

    Added a third member to ensure new threads are able to be created and played; Conductor now properly sets up and cues each thread to play!

--- TestConductor.java
+++ TestConductor.java
@@ -20,18 +20,22 @@ public class TestConductor {
         // Create members
         Member member1 = new Member("Alice", line, playQueue);
         member1.assignNotes("left", new BellNote(Note.C4, NoteLength.QUARTER));
-        member1.assignNotes("right", new BellNote(Note.E4, NoteLength.QUARTER));
+
 
         Member member2 = new Member("Bob", line, playQueue);
         member2.assignNotes("left", new BellNote(Note.G4, NoteLength.QUARTER));
 
+        Member member3 = new Member("Charlie", line, playQueue);
+        member3.assignNotes("right", new BellNote(Note.E4, NoteLength.QUARTER));
+
         // Create conductor
-        List<Member> members = Arrays.asList(member1, member2);
+        List<Member> members = Arrays.asList(member1, member2, member3);
         Conductor conductor = new Conductor(members, playQueue, 120);  // 120 BPM
 
         // Start member threads
         member1.start();
         member2.start();
+        member3.start();
 
         // Start the performance
         List<BellNote> songNotes = Arrays.asList(
@@ -48,6 +52,7 @@ public class TestConductor {
         // Interrupt members after performance
         member1.interrupt();
         member2.interrupt();
+        member3.interrupt();
         line.drain();
         line.close();
     }
