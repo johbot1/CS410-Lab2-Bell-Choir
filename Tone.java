c4b49f682bed4cb5a3cc7dc94147129b4139611b
John <jbotonakis@carroll.edu>
2025-03-23 17:11:38 -0600

    Removed BellNote from inside, and gave it its own class

--- Tone.java
+++ Tone.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import javax.sound.sampled.AudioFormat;
@@ -131,81 +132,4 @@ public class Tone {
         };
     }
 
-}
-
-class BellNote {
-    final Note note;
-    final NoteLength length;
-
-    BellNote(Note note, NoteLength length) {
-        this.note = note;
-        this.length = length;
-    }
-}
-
-enum NoteLength {
-    WHOLE(1.0f),
-    HALF(0.5f),
-    QUARTER(0.25f),
-    EIGTH(0.125f);
-
-    private final int timeMs;
-
-    private NoteLength(float length) {
-        timeMs = (int)(length * Note.MEASURE_LENGTH_SEC * 1000);
-    }
-
-    public int timeMs() {
-        return timeMs;
-    }
-}
-
-enum Note {
-    // REST Must be the first 'Note'
-    // A4 MUST be the second 'Note'
-    REST,
-    A4,
-    A4S,
-    B4,
-    C4,
-    C4S,
-    D4,
-    D4S,
-    E4,
-    F4,
-    F4S,
-    G4,
-    G4S,
-    A5;
-
-    public static final int SAMPLE_RATE = 48 * 1024; // ~48KHz
-    public static final int MEASURE_LENGTH_SEC = 1;
-
-    // Circumference of a circle divided by # of samples
-    private static final double step_alpha = (2.0d * Math.PI) / SAMPLE_RATE;
-
-    private final double FREQUENCY_A_HZ = 440.0d;
-    private final double MAX_VOLUME = 127.0d;
-
-    private final byte[] sinSample = new byte[MEASURE_LENGTH_SEC * SAMPLE_RATE];
-
-    private Note() {
-        int n = this.ordinal();
-        if (n > 0) {
-            // Calculate the frequency!
-            final double halfStepUpFromA = n - 1;
-            final double exp = halfStepUpFromA / 12.0d;
-            final double freq = FREQUENCY_A_HZ * Math.pow(2.0d, exp);
-
-            // Create sinusoidal data sample for the desired frequency
-            final double sinStep = freq * step_alpha;
-            for (int i = 0; i < sinSample.length; i++) {
-                sinSample[i] = (byte)(Math.sin(i * sinStep) * MAX_VOLUME);
-            }
-        }
-    }
-
-    public byte[] sample() {
-        return sinSample;
-    }
 }
